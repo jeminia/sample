0bd01cad175617b6ec7dcfb7b4616f3e5f489eba
Nagata Takayuki <nagatatk@gmail.com>
2017-11-02 00:30:27 +0900

    20171102 0030

--- s3test.sh
+++ s3test.sh
@@ -2,8 +2,8 @@
 
 echo "aws cli setting"
 
-#export AWS_ACCESS_KEY_ID=AKIAJV4VZ6Q6QTNS7ZMA
-#export AWS_SECRET_ACCESS_KEY=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
-#export AWS_DEFALUT_REGION=ap-northeast-1
+export AWS_ACCESS_KEY_ID=AKIAJV4VZ6Q6QTNS7ZMA
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFALUT_REGION=ap-northeast-1
 
 aws s3 ls
