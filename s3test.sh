b9a1a5a85312bacc9ee5d7c42002513e1f5faf99
Nagata Takayuki <nagatatk@gmail.com>
2017-11-02 00:09:37 +0900

    20171102 0009

--- s3test.sh
+++ s3test.sh
@@ -2,4 +2,8 @@
 
 echo "aws cli setting"
 
-#aws confiure
+export AWS_ACCESS_KEY_ID=AKIAJN4PLHTC5V7IVS2Q
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFALUT_REGION=ap-northeast-1
+
+aws s3 ls
